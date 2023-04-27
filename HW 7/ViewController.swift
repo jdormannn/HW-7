3901707c64318a30279f7dcc702ea02d77080444
jdormannn <89854560+jdormannn@users.noreply.github.com>
2023-04-27 18:11:12 -0400

    Done

--- HW 7/ViewController.swift
+++ HW 7/ViewController.swift
@@ -9,6 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var outputLabel: UILabel!
+  
+    @IBAction func foundTap(_ sender: Any) {
+        
+        outputLabel.text = "You tapped!"
+        self.view.backgroundColor =
+        UIColor.gray
+    }
+    
+    
+    @IBAction func swipeFound(_ sender: Any) {
+    
+    
+        
+        outputLabel.text = "You swiped!"
+        self.view.backgroundColor =
+        UIColor.blue
+    }
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
